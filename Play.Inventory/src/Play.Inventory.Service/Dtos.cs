daa713500cab7c363d606d4eada7fc1d8463ebad
memiadmin <xarmaaaruti@gmail.com>
2023-01-20 07:38:28 +0000

    Update Inventory Service

---
+++ Play.Inventory/src/Play.Inventory.Service/Dtos.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Play.Inventory.Service
+{
+    public record GrantItemsDto(Guid UserId, Guid CatalogItemId, int Quantity);
+    public record InventoryItemDto(Guid CatalogItemId, int Quantity, DateTimeOffset AcquiredDate);
+
+}
